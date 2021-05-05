9bf62f8fcd24cedb878e045599a6eebe78da8692
yamagata-akita <tiyduts@gmail.com>
2021-05-05 18:07:50 +0900

    modify disk.rs

--- rdbms/src/disk.rs
+++ rdbms/src/disk.rs
@@ -1,7 +1,50 @@
+use std::convert::TryInto;
+use std::fs::{File, OpenOptions};
+use std::io::{self, prelude::*, SeekFrom};
+use std::path::Path;
+
+use zerocopy::{AsBytes, FromBytes};
 
 pub const PAGE_SIZE: usize = 4096;
 
-pub sturct PageId(pub u64);
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, FromBytes, AsBytes)]
+#[repr(C)]
+pub struct PageId(pub u64);
+
+impl PageId {
+    pub const INVALID_PAGE_ID: PageId = PageId(u64::MAX);
+
+    pub fn valid(self) -> Option<PageId> {
+        if self == Self::INVALID_PAGE_ID {
+            None
+        } else {
+            Some(self)
+        }
+    }
+
+    pub fn to_u64(self) -> u64 {
+        self.0
+    }
+}
+
+impl Default for PageId {
+    fn default() -> Self {
+        Self::INVALID_PAGE_ID
+    }
+}
+
+impl From<Option<PageId>> for PageId {
+    fn from(page_id: Option<PageId>) -> Self {
+        page_id.unwrap_or_default()
+    }
+}
+
+impl From<&[u8]> for PageId {
+    fn from(bytes: &[u8]) -> Self {
+        let arr = bytes.try_into().unwrap();
+        PageId(u64::from_ne_bytes(arr))
+    }
+}
 
 pub struct DiskManager {
     // ヒープファイルのファイルディスクリプタ
@@ -30,7 +73,7 @@ impl DiskManager {
             .write(true)
             .create(true)
             .open(heap_file_path)?;
-        Self::new(heap_file);
+        Self::new(heap_file)
     }
 
     // 新しいページIDを採番する
@@ -41,7 +84,7 @@ impl DiskManager {
     }
 
     // ページのデータを読み出す
-    pub fn read_page_data(&mut self, page_Id: PageId, data: &mut [u8]) -> io::Result<()> {
+    pub fn read_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> io::Result<()> {
         // オフセットを計算
         let offset = PAGE_SIZE as u64 * page_id.to_u64();
 
@@ -62,5 +105,10 @@ impl DiskManager {
 
         // データを書き込む
         self.heap_file.write_all(data)
+    }
+
+    pub fn sync(&mut self) -> io::Result<()> {
+        self.heap_file.flush()?;
+        self.heap_file.sync_all()
     }
 }
