14d65b72726870096f5287b6c7a4673753c6ef24
yamagata-akita <tiyduts@gmail.com>
2021-05-06 20:51:23 +0900

    add test

--- rdbms/src/buffer.rs
+++ rdbms/src/buffer.rs
@@ -45,6 +45,7 @@ pub struct Frame {
 }
 
 // バッファプール
+#[derive(Debug)]
 pub struct BufferPool {
     buffers: Vec<Frame>,
     next_victim_id: BufferId,
@@ -114,6 +115,7 @@ impl IndexMut<BufferId> for BufferPool {
 }
 
 // バッファプールマネージャ
+#[derive(Debug)]
 pub struct BufferPoolManager {
     // バッファプール内に必要なページのキャッシュがない場合、ディスクマネージャを呼び出して
     // ヒープファイルからデータを読み込む
@@ -152,7 +154,7 @@ impl BufferPoolManager {
 
             // 2.捨てるバッファのis_dirtyフラグがtrueなら、そのバッファをディスクに書き出す。
             if buffer.is_dirty.get() {
-                self.disk.write_page_data(page_id, buffer.page.get_mut())?;
+                self.disk.write_page_data(evict_page_id, buffer.page.get_mut())?;
             }
             buffer.page_id = page_id;
             buffer.is_dirty.set(false);
@@ -167,5 +169,101 @@ impl BufferPoolManager {
         self.page_table.remove(&evict_page_id);
         self.page_table.insert(page_id, buffer_id);
         Ok(page)
+    }
+
+    pub fn create_page(&mut self) -> Result<Rc<Buffer>, Error> {
+        let buffer_id = self.pool.evict().ok_or(Error::NoFreeBuffer)?;
+        let frame = &mut self.pool[buffer_id];
+        let evict_page_id = frame.buffer.page_id;
+        let page_id = {
+            let buffer = Rc::get_mut(&mut frame.buffer).unwrap();
+            if buffer.is_dirty.get() {
+                self.disk.write_page_data(evict_page_id, buffer.page.get_mut())?;
+            }
+            self.page_table.remove(&evict_page_id);
+            let page_id = self.disk.allocate_page();
+            *buffer = Buffer::default();
+            buffer.page_id = page_id;
+            buffer.is_dirty.set(true);
+            frame.usage_count = 1;
+            page_id
+        };
+        let page = Rc::clone(&frame.buffer);
+        self.page_table.remove(&evict_page_id);
+        self.page_table.insert(page_id, buffer_id);
+        Ok(page)
+    }
+
+    pub fn flush(&mut self) -> Result<(), Error> {
+        for (&page_id, &buffer_id) in self.page_table.iter() {
+            let frame = &self.pool[buffer_id];
+            let mut page = frame.buffer.page.borrow_mut();
+            self.disk.write_page_data(page_id, page.as_mut())?;
+            frame.buffer.is_dirty.set(false);
+        }
+        self.disk.sync()?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempfile::tempfile;
+
+    #[test]
+    fn test() {
+        let mut hello = Vec::with_capacity(PAGE_SIZE);
+        hello.extend_from_slice(b"hello");
+        hello.resize(PAGE_SIZE, 0);
+        let mut world = Vec::with_capacity(PAGE_SIZE);
+        world.extend_from_slice(b"world");
+        world.resize(PAGE_SIZE, 0);
+
+        let disk = DiskManager::new(tempfile().unwrap()).unwrap();
+        let pool = BufferPool::new(1);
+        let mut bufmgr = BufferPoolManager::new(disk, pool);
+
+        let page1_id = {
+            let buffer = bufmgr.create_page().unwrap();
+            assert!(bufmgr.create_page().is_err());
+            let mut page = buffer.page.borrow_mut();
+            page.copy_from_slice(&hello);
+            buffer.is_dirty.set(true);
+            buffer.page_id
+        };
+
+        {
+            // ページ1のチェック
+            let buffer = bufmgr.fetch_page(page1_id).unwrap();
+            let page = buffer.page.borrow();
+            assert_eq!(&hello, page.as_ref());
+        }
+
+        // println!("チェック後: {:?}", bufmgr);
+
+        let page2_id = {
+            let buffer = bufmgr.create_page().unwrap();
+            let mut page = buffer.page.borrow_mut();
+            page.copy_from_slice(&world);
+            buffer.is_dirty.set(true);
+            buffer.page_id
+        };
+
+        // println!("再チェック前: {:?}", bufmgr);
+
+        {
+            // ページ1の再チェック(ディスクに退避させられているのを確認)
+            let buffer = bufmgr.fetch_page(page1_id).unwrap();
+            let page = buffer.page.borrow();
+            assert_eq!(&hello, page.as_ref());
+        }
+
+        {
+            // ページ2のチェック
+            let buffer = bufmgr.fetch_page(page2_id).unwrap();
+            let page = buffer.page.borrow();
+            assert_eq!(&world, page.as_ref());
+        }
     }
 }
