0a37fdf131f693cb4b438b3bdf0b42ba55031b45
yamagata-akita <tiyduts@gmail.com>
2021-05-13 19:48:42 +0900

    table(select)

---
+++ rdbms/examples/simple-table-all.rs
@@ -0,0 +1,23 @@
+use anyhow::Result;
+
+use rdbms::btree::{BTree, SearchMode};
+use rdbms::buffer::{BufferPool, BufferPoolManager};
+use rdbms::disk::{DiskManager, PageId};
+use rdbms::tuple;
+
+fn main() -> Result<()> {
+    let disk = DiskManager::open("simple.rly")?;
+    let pool = BufferPool::new(10);
+    let mut bufmgr = BufferPoolManager::new(disk, pool);
+
+    let btree = BTree::new(PageId(0));
+    let mut iter = btree.search(&mut bufmgr, SearchMode::Start)?;
+
+    while let Some((key, value)) = iter.next(&mut bufmgr)? {
+        let mut record = vec![];
+        tuple::decode(&key, &mut record);
+        tuple::decode(&value, &mut record);
+        println!("{:?}", tuple::Pretty(&record));
+    }
+    Ok(())
+}
